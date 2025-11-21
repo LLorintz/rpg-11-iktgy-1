e9f9f335864479fe78cce0711b6bac477b23bd61
LLorintz <lorintz.levente@mechwart.com>
2025-11-21 08:29:04 +0100

    Add files via upload

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -5,6 +5,36 @@ public partial class Player : CharacterBody2D
 {
     [Export]private float MoveSpeed = 200f;
 
+    private AnimatedSprite2D animatedsprite;
+
+    public override void _Ready()
+    {
+        animatedsprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
+
+    private void HandleAnimation(Vector2 direction){
+
+        if(direction == Vector2.Zero)
+        {
+            animatedsprite.Stop();
+            return;
+        }
+
+        string anim = "";
+
+        if(direction.X !=0){
+            anim=direction.X>0 ? "walkright" : "walkleft";
+        }
+        else if(direction.Y != 0)
+        {
+            anim=direction.Y>0? "walkdown" : "walkup";
+        }
+
+        if(animatedsprite.Animation != anim){
+            animatedsprite.Play(anim);
+        }
+    }
+
     private void GetInput()
     {
         Vector2 inputDirection = Vector2.Zero;
@@ -36,7 +66,10 @@ public partial class Player : CharacterBody2D
         
         Velocity = inputDirection * MoveSpeed;
 
+        HandleAnimation(inputDirection);
     }
+
+
 
     public override void _PhysicsProcess(double delta)
     {
